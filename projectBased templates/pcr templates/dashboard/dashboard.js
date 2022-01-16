5e7d609aa3d2c5346ef4d92a868b04fabc255e37
jahir-raihan <raihan.joy10@gmail.com>
2022-01-16 21:26:51 +0600

    added dashboard system

---
+++ projectBased templates/pcr templates/dashboard/dashboard.js
@@ -0,0 +1,16 @@
+function off(){
+    console.log('button clicked')
+    document.getElementById('overlay1').style.display = "none";
+}
+function on(){
+    console.log('button clicked')
+    document.getElementById('overlay1').style.display = "block";
+}
+function off1(){
+    console.log('button clicked')
+    document.getElementById('overlay3').style.display = "none";
+}
+function on1(){
+    console.log('button clicked')
+    document.getElementById('overlay3').style.display = "block";
+}
